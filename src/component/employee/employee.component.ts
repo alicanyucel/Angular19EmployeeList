b14882650b6b2a988db64825709eb68eacd59c8a
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-01-13 22:23:13 +0300

    tamamlanacak

---
+++ src/component/employee/employee.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+@Component({
+  selector: 'app-employee',
+  imports: [MatCardModule],
+  templateUrl: './employee.component.html',
+  styleUrl: './employee.component.css'
+})
+export class EmployeeComponent {
+
+}
