3503bafaf5a99a752504ae206ff83d9b6dd95dd8
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-01-20 00:48:52 +0300

    tmam ya

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,13 +5,13 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { employeeReducer } from './store/Employee.Reducer';
 import { empEffects } from './store/Employee.Effects';
 import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(),
+  providers: [provideHttpClient(withFetch()),provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(),
     provideHttpClient(), provideToastr(), 
     provideStore({'emp':employeeReducer}), provideEffects([empEffects]), 
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })]
