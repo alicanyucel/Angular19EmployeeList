7bf0fc02ae11f8aa83cb24b2ad374ec79e6bc235
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-01-20 00:42:36 +0300

    çözdüm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { AfterViewInit, ChangeDetectorRef, Component, effect, ElementRef, model,
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { RouterOutlet } from '@angular/router';
 import { ChildComponent } from './component/child/child.component';
 import { TempchildComponent } from './component/tempchild/tempchild.component';
 import { FormsModule } from '@angular/forms';
@@ -10,20 +9,27 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [MatFormFieldModule, MatCardModule, MatInputModule,
-    ChildComponent, TempchildComponent, FormsModule, CommonModule],
+  imports: [ MatFormFieldModule, MatCardModule, MatInputModule,
+    ChildComponent,TempchildComponent,FormsModule,CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements AfterViewInit {
 
-  
+  //@ViewChild('name') nameElement!: ElementRef;
+  // @ViewChild(ChildComponent) child!: ChildComponent;
+  //@ViewChildren(ChildComponent) childs!: QueryList<ChildComponent>;
+
+  // nameElement = viewChild.required<ElementRef>('name');
+  // child = viewChild.required<ChildComponent>(ChildComponent);
+  // childs = viewChildren(ChildComponent);
   
   readonly nameElement = viewChild.required<ElementRef>('name');
   readonly child = viewChild.required(ChildComponent);
   readonly childs = viewChildren(ChildComponent);
 
-  
+  //username='Test'
   username=model<string>('Test')
   userobj=model<{firstname:string,lastname:string}>({
     firstname: '',
@@ -39,17 +45,33 @@ export class AppComponent implements AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-   
+    // console.log(this.nameElement);
+    // this.nameElement.nativeElement.focus();
+    // this.nameElement.nativeElement.value = '200';
+    // this.nameElement.nativeElement.disabled = 'disabled'
+
+    // let _element = this.nameElement();
+    // _element.nativeElement.focus();
+    // _element.nativeElement.value = '300'
+    // _element.nativeElement.disabled = 'diabled'
 
     this.nameElement().nativeElement.focus();
     const nameElement = this.nameElement();
     nameElement.nativeElement.value = '200';
     nameElement.nativeElement.disabled = 'disabled'
 
-    
+    //this.child.title = 'Child'
+    //this.child().title = 'Child'
     this.child().title = 'Child'
 
-    
+    //console.log(this.childs.toArray())
+    // this.childs.toArray().forEach((item, i) => {
+    //   item.title = 'Child ' + i
+    // })
+
+    // this.childs().forEach((item, i) => {
+    //   item.title = 'Child ' + i
+    // })
 
     this.childs().forEach((item, i) => {
       item.title = 'Child ' + i
